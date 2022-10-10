eb6001634151d12d8dc941b5c02b69b083fdca67
Garvita K Basantani <garvitakbasantani@gmail.com>
2022-10-10 17:48:47 +0530

    create daily task post api and task serializer

--- server/projects/views.py
+++ server/projects/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from datetime import datetime
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Daily
+from .serializers import TaskSerializer
+
+
+class DailyTask(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request):
+        user = request.user
+        date = datetime.date(datetime.today())  # Today's date (UTC)
+
+        # Check whether daily data exists for the user for current date, if not create one
+        try:
+            daily_obj, created = Daily.objects.get_or_create(
+                user=user, date=date)
+        except Exception as e:
+            return Response({'error': 'Cannot get or create Daily model object', 'message': 'Unable to add task, please try again later!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # Create a new task for the user
+        task_data = request.data
+        task_data['start_date'] = date
+        task_data['due_date'] = date
+        task_data['daily'] = daily_obj.id
+
+        serializer = TaskSerializer(data=task_data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response({'task': serializer.data}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            if serializer.errors:
+                return Response({'error': serializer.errors, 'message': 'Something went wrong. Please try again in sometime'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': str(e), 'message': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
